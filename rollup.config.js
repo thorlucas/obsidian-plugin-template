5a727a8d2e09945fd3dbaf8e152abb56274496de
Thor Correia <thor.lucas@icloud.com>
2021-10-09 06:35:16 -0700

    building to custom vaults

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,18 @@
 import typescript from '@rollup/plugin-typescript';
 import {nodeResolve} from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
+import copy from 'rollup-plugin-copy';
+import dotenv from 'dotenv';
+import manifest from './manifest.json';
+
+dotenv.config();
+
+const pluginName = manifest['id'];
 
 const isProd = (process.env.BUILD === 'production');
+
+const vault = process.env.TEST_VAULT || './vault';
+const pluginDir = `${vault}/.obsidian/plugins/${pluginName}`;
 
 const banner = 
 `/*
@@ -12,19 +22,25 @@ if you want to view the source visit the plugins github repository
 `;
 
 export default {
-  input: 'main.ts',
-  output: {
-    dir: '.',
-    sourcemap: 'inline',
-    sourcemapExcludeSources: isProd,
-    format: 'cjs',
-    exports: 'default',
-    banner,
-  },
-  external: ['obsidian'],
-  plugins: [
-    typescript(),
-    nodeResolve({browser: true}),
-    commonjs(),
-  ]
+	input: 'src/main.ts',
+	output: {
+		dir: pluginDir,
+		sourcemap: 'inline',
+		sourcemapExcludeSources: isProd,
+		format: 'cjs',
+		exports: 'default',
+		banner,
+	},
+	external: ['obsidian'],
+	plugins: [
+		typescript(),
+		nodeResolve({browser: true}),
+		commonjs(),
+		copy({
+			targets: [
+				{ src: 'manifest.json', dest: pluginDir },
+				{ src: 'styles.css', dest: pluginDir },
+			],
+		}),
+	]
 };
