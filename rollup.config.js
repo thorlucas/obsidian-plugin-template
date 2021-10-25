d787dc92b7c96ea98cb251ab41372e275f778d79
Thor Correia <thor.lucas@berkeley.edu>
2021-10-25 06:55:16 -0700

    test vault settings

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,17 @@
 import typescript from '@rollup/plugin-typescript';
 import {nodeResolve} from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+// TODO: Get from manifest
+const pluginName = 'obsidian-plugin-template';
 
 const isProd = (process.env.BUILD === 'production');
+
+const vault = process.env.TEST_VAULT || './vault';
+const pluginDir = `${vault}/.obsidian/plugins/${pluginName}`;
 
 const banner = 
 `/*
@@ -12,19 +21,19 @@ if you want to view the source visit the plugins github repository
 `;
 
 export default {
-  input: 'main.ts',
-  output: {
-    dir: '.',
-    sourcemap: 'inline',
-    sourcemapExcludeSources: isProd,
-    format: 'cjs',
-    exports: 'default',
-    banner,
-  },
-  external: ['obsidian'],
-  plugins: [
-    typescript(),
-    nodeResolve({browser: true}),
-    commonjs(),
-  ]
+	input: 'src/main.ts',
+	output: {
+		dir: pluginDir,
+		sourcemap: 'inline',
+		sourcemapExcludeSources: isProd,
+		format: 'cjs',
+		exports: 'default',
+		banner,
+	},
+	external: ['obsidian'],
+	plugins: [
+		typescript(),
+		nodeResolve({browser: true}),
+		commonjs(),
+	]
 };
